a5485b7b223bdb5e94f9155d7a37bbf382bc04f4
Alexandre Aguiar <venom4008@gmail.com>
2025-05-16 16:30:44 -0300

    Modificação de saída de URL para nome da fila

--- ListQueuesWithMessages.ps1
+++ ListQueuesWithMessages.ps1
@@ -13,19 +13,20 @@ Write-Output "TOTAL DE FILAS: $total"
 
 $queueData | ForEach-Object -Parallel {
     $url = $_.Url
+    $queueName = $url -replace "https://sqs.sa-east-1.amazonaws.com/381492245517/", ""
     $index = $_.Index
     $totalQueues = $_.TotalQueues
 
     $msgCount = aws sqs get-queue-attributes --queue-url $url --attribute-names ApproximateNumberOfMessages --query "Attributes.ApproximateNumberOfMessages" --output text
     if ($msgCount -gt 0) {
-        Write-Host "$index. $url ($msgCount mensagens)" -ForegroundColor Green
+        Write-Host "$index. $queueName ($msgCount mensagens)" -ForegroundColor Green
     } else {
-        Write-Host "[------------VAZIA------------]$index. $url"
+        Write-Host "[------------VAZIA------------]$index. $queueName"
     }
 
     $currentProgress = [Math]::Round((($index + 1) * 100)/$totalQueues, 2) 
     Write-Progress -Activity "Search in Progress" -Status "$currentProgress% Complete:" -PercentComplete $currentProgress
-} -ThrottleLimit  10
+} -ThrottleLimit 10
 
 $elapsedTime = $(Get-Date) - $startTime
 $totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
